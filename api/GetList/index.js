8c8fcdac258b50c94dc69ec344130199e44f8a38
BrandonHopcraft <59028011+BrandonHopcraft@users.noreply.github.com>
2020-04-21 15:25:58 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -4,6 +4,6 @@ module.exports = async function (context, req) {
     let isPrime = (a,i = 2) => a <= i ? true : (a % i !== 0 && isPrime(a,i+1))
     
     context.res = {
-        body: new Array(100).fill(0).map((a,b) => b).filter((a) => isPrime(a))
+        body: new Array(100).fill(0).map((a,b) => b)
     }
 };
