1111d3b51311aa20fd4e3eb897fc99a12e50a2b1
BrandonHopcraft <59028011+BrandonHopcraft@users.noreply.github.com>
2020-04-21 15:17:33 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
-    let isPrime = (a,i = 2) => i <= a ? true : (a % i !== 0 && isPrime(a,i+1))
+    let isPrime = (a,i = 2) => a <= i ? true : (a % i !== 0 && isPrime(a,i+1))
     
     context.res = {
         body: new Array(100).fill(0).map((a,b) => b).filter((a) => isPrime(a))
